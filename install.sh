23d339cf2502c8387971c0d16c2fa7688d8315a9
Thakor Rohan <67776672+ThakorRohan@users.noreply.github.com>
2022-06-24 23:30:54 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update -y && sudo apt upgrade -y
+sudo systemctl stop mysql.service
+sudo systemctl stop apache2.service
+sudo systemctl stop dovecot.service
+sudo systemctl stop postfix.service
+sudo apt-get remove -y mysql-server apache2 dovecot-core postfix
+cd /opt
+wget -O installer.sh https://cyberpanel.net/install.sh
+chmod +x installer.sh
+sh installer.sh
